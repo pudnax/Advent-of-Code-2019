654dc5adddb7ec2cec2d9ba9b252af2669781a42
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-05 11:25:51 +0500

    Data from day3

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -25,10 +25,20 @@ fn target_02(c: &mut Criterion) {
     });
 }
 
+fn target_03(c: &mut Criterion) {
+    let day03 = fs::read_to_string("data/day3").unwrap();
+    c.bench_function("day_03", |b| {
+        b.iter(|| {
+            let reader = io::BufReader::new(day03.as_bytes());
+            aoc2019::day02::run(reader).unwrap();
+        })
+    });
+}
+
 criterion_group! {
     name = group;
     config = Criterion::default().warm_up_time(Duration::from_secs(5));
-    targets = target_01, target_02
+    targets = target_01, target_02,target_03
 }
 
 criterion_main!(group);
