f8429a3b6e5269e4db4f29ad6db89dd689fe4e26
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-10 00:03:09 +0500

    Day8 part 1

--- src/day08.rs
+++ src/day08.rs
@@ -6,12 +6,35 @@ pub fn run<R>(input: R) -> Result<(String, String), Error>
 where
     R: io::BufRead,
 {
-    let ans = parse_input(input, 25, 6)?;
+    let ans = parse_input(input)?;
+    let ans = construct_matrix(&ans, 25, 6)?;
+    let ans = find_uncorrupted(ans)?;
     println!("{:?}", ans);
     Ok(("ans".to_string(), "bar".to_string()))
 }
 
-fn parse_input<R>(mut input: R, w: usize, h: usize) -> Result<Vec<u8>, Error>
+fn find_uncorrupted(arr: Vec<&[u8]>) -> Result<usize, Error> {
+    let min_index = arr
+        .iter()
+        .enumerate()
+        .min_by_key(|(_, &arr)| arr.iter().filter(|&&x| x == 0).count())
+        .ok_or_else(|| error!("Unable find array with 0"))?
+        .0;
+    let ones = arr[min_index].iter().filter(|&&x| x == 1).count();
+    let twos = arr[min_index].iter().filter(|&&x| x == 2).count();
+
+    Ok(ones * twos)
+}
+
+fn construct_matrix(arr: &[u8], w: usize, h: usize) -> Result<Vec<&[u8]>, Error> {
+    if arr.len() % (w * h) != 0 {
+        bail!("Uncorrect array size");
+    }
+
+    Ok(arr.chunks(w * h).collect::<Vec<_>>())
+}
+
+fn parse_input<R>(mut input: R) -> Result<Vec<u8>, Error>
 where
     R: io::BufRead,
 {
