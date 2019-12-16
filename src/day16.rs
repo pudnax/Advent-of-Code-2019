d5ec8997bcf7d088b38652bfd3324a02dd89d8c2
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-16 20:32:42 +0500

    Day16 parse_input

--- src/day16.rs
+++ src/day16.rs
@@ -2,11 +2,25 @@ use std::io;
 
 use crate::error::Error;
 
-pub fn run<R>(_reader: R) -> Result<(String, String), Error>
+pub fn run<R>(reader: R) -> Result<(String, String), Error>
 where
     R: io::BufRead,
 {
+    let signal = parse_input(reader)?;
+    println!("{:?}", signal);
     Ok(("answer1".to_string(), "answer2".to_string()))
+}
+
+fn parse_input<R>(mut reader: R) -> Result<Vec<u8>, Error>
+where
+    R: io::BufRead,
+{
+    let mut buff = Vec::new();
+    reader.read_to_end(&mut buff)?;
+    buff.pop();
+    buff.iter_mut().for_each(|b| *b -= 48);
+
+    Ok(buff)
 }
 
 #[cfg(test)]
