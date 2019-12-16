e4c875e838d4d5e7111daaf5da286cab3bafbe36
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-16 21:56:16 +0500

    iters

--- src/day16.rs
+++ src/day16.rs
@@ -8,7 +8,43 @@ where
 {
     let signal = parse_input(reader)?;
     println!("{:?}", signal);
+
+    let mut fft = FFT::new(&[0, 1, 0, -1]);
+    fft.step();
+    fft.step();
+    let cycled = signal.iter().zip(fft.into_iter().cycle());
+    println!("{:?}", cycled.collect::<Vec<_>>());
     Ok(("answer1".to_string(), "answer2".to_string()))
+}
+
+struct FFT<'a> {
+    pattern: &'a [i64],
+    phase: usize,
+}
+
+impl<'a> FFT<'a> {
+    fn new(pattern: &[i64]) -> FFT {
+        FFT { pattern, phase: 1 }
+    }
+
+    fn step(&mut self) {
+        self.phase += 1;
+    }
+}
+
+impl<'a> IntoIterator for FFT<'a> {
+    type Item = i64;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+    fn into_iter(self) -> Self::IntoIter {
+        let mut iter = self
+            .pattern
+            .iter()
+            .flat_map(|x| vec![*x; self.phase])
+            .collect::<Vec<_>>()
+            .into_iter();
+        iter.next();
+        iter
+    }
 }
 
 fn parse_input<R>(mut reader: R) -> Result<Vec<u8>, Error>
