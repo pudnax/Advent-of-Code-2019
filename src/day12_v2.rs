f1cc0e2bebcbeb6a74cde6fcaaf255e7ce955360
Alex Komissarov <k.a.komissar@gmail.com>
2019-12-18 17:44:04 +0500

    +

--- src/day12_v2.rs
+++ src/day12_v2.rs
@@ -1,34 +1,49 @@
 use std::cell::RefCell;
 use std::io;
+use std::mem;
+use std::ops::{Deref, DerefMut};
 
 use crate::error::Error;
 use crate::utils::Vec3;
-pub fn run<R>(reader: R) -> std::result::Result<(String, String), Error>
+
+#[cfg(not(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "avx2"
+)))]
+use self::normal::Moon;
+
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "avx2"
+))]
+use self::simd::Moon;
+
+pub fn run<R>(reader: R) -> Result<(String, String), Error>
 where
     R: io::BufRead,
 {
     let mut moons = parse_input(reader)?;
-    for _ in 0..100 {
+    for _ in 0..1_000 {
         moons.step();
     }
     let answer1 = moons.energy();
-    Ok((answer1.to_string(), "answer2".to_string()))
+    Ok((answer1.to_string(), "TODO".to_string()))
 }
 
 fn parse_input<R>(reader: R) -> Result<Moons, Error>
 where
     R: io::BufRead,
 {
-    let mut moons: [RefCell<Moon>; 4] = unsafe { std::mem::MaybeUninit::zeroed().assume_init() }; // undefined behavior!
-    let mut count = 0;
+    // safety: TODO
+    let mut moons: [RefCell<Moon>; 4] = unsafe { mem::MaybeUninit::uninit().assume_init() };
+    let mut i = 0;
     for res in reader.lines() {
-        if count > 3 {
-            bail!("Can only support exactly 4 moons")
+        if i > 3 {
+            bail!("Can only support exactly 4 moons.");
         }
         let line = res?;
         let line = line.trim();
-
-        let mut pos: [i64; 3] = unsafe { std::mem::MaybeUninit::zeroed().assume_init() }; // undefined behavior!
+        let mut pos: [i64; 3] = [0i64; 3];
         let mut j = 0;
         for part in line.split(',') {
             let coord = part
@@ -43,24 +58,20 @@ where
             j += 1;
         }
         if j != 3 {
-            bail!("Found too many coordinates")
+            bail!("Found too many coordinates.");
         }
-
-        let moon = Moon {
-            pos: pos.into(),
-            vel: Vec3::new(0, 0, 0),
-        };
-        moons[count] = RefCell::new(moon);
-        count += 1;
+        let moon = Moon::new(pos, Vec3::default());
+        moons[i] = RefCell::new(moon);
+        i += 1;
     }
-    if count != 4 {
-        bail!("Can only exactly support 4 moons")
+    if i != 4 {
+        bail!("Can only support exactly 4 moons");
     }
 
     Ok(Moons(moons))
 }
 
-#[derive(Clone, Eq, PartialEq, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Moons([RefCell<Moon>; 4]);
 
 impl Moons {
@@ -68,77 +79,207 @@ impl Moons {
         let mut total = 0;
         for moon in self.iter() {
             let moon = moon.borrow();
-            let mut pot_energy = 0;
-            let mut kin_energy = 0;
+            let mut potential = 0;
+            let mut kinetic = 0;
             for k in 0..3 {
-                pot_energy += moon.pos[k].abs() as u64;
-                kin_energy += moon.vel[k].abs() as u64;
+                potential += moon.pos()[k].abs() as u64;
+                kinetic += moon.vel()[k].abs() as u64;
             }
-            total += pot_energy * kin_energy;
+            total += potential * kinetic;
         }
         total
     }
-
-    fn step(&mut self) {
-        for moon_i in &self.0 {
-            for moon_j in &self.0 {
-                if moon_i == moon_j {
-                    continue;
-                }
-                for k in 0..3 {
-                    let pos_i = moon_i.borrow().pos()[k];
-                    let pos_j = moon_j.borrow().pos()[k];
-                    if pos_i < pos_j {
-                        moon_i.borrow_mut().vel_mut()[k] += 1;
-                    } else if pos_i > pos_j {
-                        moon_i.borrow_mut().vel_mut()[k] -= 1;
-                    }
-                }
-            }
-        }
-        for moon in self.iter_mut() {
-            for k in 0..3 {
-                let vel = moon.borrow().vel()[k];
-                moon.borrow_mut().pos_mut()[k] += vel;
-            }
-        }
-    }
 }
 
-impl std::ops::Deref for Moons {
+impl Deref for Moons {
     type Target = [RefCell<Moon>];
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl std::ops::DerefMut for Moons {
+impl DerefMut for Moons {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
     }
 }
 
-#[derive(Copy, Clone, Eq, Default, PartialEq, Debug)]
-struct Moon {
-    pos: Vec3<i64>,
-    vel: Vec3<i64>,
+#[cfg(not(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "avx2"
+)))]
+mod normal {
+    use super::*;
+
+    impl Moons {
+        pub(crate) fn step(&mut self) {
+            for moon_i in &self.0 {
+                for moon_j in &self.0 {
+                    if moon_i == moon_j {
+                        continue;
+                    }
+                    for k in 0..3 {
+                        let pos_i = moon_i.borrow().pos()[k];
+                        let pos_j = moon_j.borrow().pos()[k];
+                        if pos_i < pos_j {
+                            moon_i.borrow_mut().vel_mut()[k] += 1;
+                        } else if pos_i > pos_j {
+                            moon_i.borrow_mut().vel_mut()[k] -= 1;
+                        }
+                    }
+                }
+            }
+            for moon in self.iter_mut() {
+                for k in 0..3 {
+                    let vel = { moon.borrow().vel()[k] };
+                    moon.borrow_mut().pos_mut()[k] += vel;
+                }
+            }
+        }
+    }
+
+    #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
+    pub(crate) struct Moon {
+        pos: Vec3<i64>,
+        vel: Vec3<i64>,
+    }
+
+    impl Moon {
+        pub(crate) fn new<V, U>(pos: V, vel: U) -> Self
+        where
+            V: Into<Vec3<i64>>,
+            U: Into<Vec3<i64>>,
+        {
+            Self {
+                pos: pos.into(),
+                vel: vel.into(),
+            }
+        }
+
+        pub(crate) fn pos(&self) -> &Vec3<i64> {
+            &self.pos
+        }
+
+        pub(crate) fn pos_mut(&mut self) -> &mut Vec3<i64> {
+            &mut self.pos
+        }
+
+        pub(crate) fn vel(&self) -> &Vec3<i64> {
+            &self.vel
+        }
+
+        pub(crate) fn vel_mut(&mut self) -> &mut Vec3<i64> {
+            &mut self.vel
+        }
+    }
 }
 
-impl Moon {
-    pub(crate) fn pos(&self) -> &Vec3<i64> {
-        &self.pos
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "avx2"
+))]
+mod simd {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    use lazy_static::lazy_static;
+
+    use super::*;
+
+    lazy_static! {
+        static ref ONE: __m256i = unsafe { _mm256_set_epi64x(1, 1, 1, 0) };
+        static ref NEGATIVE_ONE: __m256i = unsafe { _mm256_set_epi64x(-1, -1, -1, 0) };
     }
 
-    pub(crate) fn pos_mut(&mut self) -> &mut Vec3<i64> {
-        &mut self.pos
+    impl Moons {
+        pub(crate) fn step(&mut self) {
+            for moon_i in &self.0 {
+                for moon_j in &self.0 {
+                    if moon_i == moon_j {
+                        continue;
+                    }
+
+                    let pos_i = moon_i.borrow().pos;
+                    let pos_j = moon_j.borrow().pos;
+
+                    // Adding
+                    let mask_gt = unsafe { _mm256_cmpgt_epi64(pos_i, pos_j) };
+                    let operand_add = unsafe { _mm256_and_si256(mask_gt, *NEGATIVE_ONE) };
+
+                    // Subtracting
+                    let mask_lt = unsafe { _mm256_cmpgt_epi64(pos_j, pos_i) };
+                    let operand_sub = unsafe { _mm256_and_si256(mask_lt, *ONE) };
+
+                    let operand = unsafe { _mm256_or_si256(operand_add, operand_sub) };
+
+                    let mut moon_ref = moon_i.borrow_mut();
+                    let vel_ref = moon_ref.vel_mut();
+                    *vel_ref = unsafe { _mm256_add_epi64(*vel_ref, operand) };
+                }
+            }
+            for moon in self.iter_mut() {
+                let new_pos = {
+                    let moon = moon.borrow();
+                    unsafe { _mm256_add_epi64(moon.pos, moon.vel) }
+                };
+                let mut moon = moon.borrow_mut();
+                *moon.pos_mut() = new_pos;
+            }
+        }
     }
 
-    pub(crate) fn vel(&self) -> &Vec3<i64> {
-        &self.vel
+    #[derive(Copy, Clone, Debug)]
+    pub(crate) struct Moon {
+        pos: __m256i,
+        vel: __m256i,
     }
 
-    pub(crate) fn vel_mut(&mut self) -> &mut Vec3<i64> {
-        &mut self.vel
+    impl PartialEq for Moon {
+        fn eq(&self, other: &Moon) -> bool {
+            self.pos() == other.pos() && self.vel() == other.vel()
+        }
+    }
+
+    impl Eq for Moon {}
+
+    impl Moon {
+        pub(crate) fn new<V, U>(pos: V, vel: U) -> Self
+        where
+            V: Into<Vec3<i64>>,
+            U: Into<Vec3<i64>>,
+        {
+            let pos = {
+                let pos = pos.into();
+                unsafe { _mm256_set_epi64x(pos.x(), pos.y(), pos.z(), 0) }
+            };
+            let vel = {
+                let vel = vel.into();
+                unsafe { _mm256_set_epi64x(vel.x(), vel.y(), vel.z(), 0) }
+            };
+            Self { pos, vel }
+        }
+
+        pub(crate) fn pos(&self) -> Vec3<i64> {
+            let mut a: [i64; 4] = unsafe { mem::MaybeUninit::uninit().assume_init() };
+            unsafe { _mm256_storeu_si256(&mut a as *mut _ as *mut _, self.pos) };
+            Vec3::new(a[3], a[2], a[1])
+        }
+
+        pub(crate) fn pos_mut(&mut self) -> &mut __m256i {
+            &mut self.pos
+        }
+
+        pub(crate) fn vel(&self) -> Vec3<i64> {
+            let mut a: [i64; 4] = unsafe { mem::MaybeUninit::uninit().assume_init() };
+            unsafe { _mm256_storeu_si256(&mut a as *mut _ as *mut _, self.vel) };
+            Vec3::new(a[3], a[2], a[1])
+        }
+
+        pub(crate) fn vel_mut(&mut self) -> &mut __m256i {
+            &mut self.vel
+        }
     }
 }
 
@@ -149,7 +290,7 @@ mod tests {
     use crate::utils;
 
     #[test]
-    fn test_14() {
-        utils::tests::test_full_problem(9, run, "3460311188", "42202");
+    fn test_12() {
+        utils::tests::test_full_problem(12, run, "7722", "TODO");
     }
 }
